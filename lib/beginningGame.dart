be553119f51fa054e52b43cf94a19a617980705f
emuth <emuth@umich.edu>
2020-10-11 22:06:49 -0400

    changes for Monday to home screen

--- lib/beginningGame.dart
+++ lib/beginningGame.dart
@@ -10,13 +10,36 @@ class _InstructionsState extends State<Instructions> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/beginningGameBG.jpg"),
-            //fit: BoxFit.cover,
+      body: Center(
+        child: Stack(children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/beginningGameBG.jpg"),
+                fit: BoxFit.cover,
+                colorFilter: new ColorFilter.mode(
+                    Colors.black.withOpacity(0.3), BlendMode.darken),
+              ),
+            ),
           ),
-        ),
+          Container(
+            alignment: Alignment.center,
+            child: Text('Welcome!',
+                style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.white)),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: IconButton(
+              icon: Icon(Icons.help, color: Colors.redAccent),
+              iconSize: 50,
+              onPressed: () {},
+            ),
+          )
+        ]),
       ),
       bottomNavigationBar: navBar(),
     );
