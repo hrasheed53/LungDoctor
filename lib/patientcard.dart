ab7f32f00886dbd897476f6762af00845076cffd
sophiesteiner <43256760+sophiesteiner@users.noreply.github.com>
2020-10-19 23:53:43 -0400

    Update patientcard.dart

--- lib/patientcard.dart
+++ lib/patientcard.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart'
+import 'package:flutter/rendering.dart';
 import 'package:path/path.dart';
 import 'testResults.dart';
 import 'gamePlay.dart';
