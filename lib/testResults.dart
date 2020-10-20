3098b1cde9f86df4414544df4e78535bd1d422ba
sophiesteiner <43256760+sophiesteiner@users.noreply.github.com>
2020-10-20 09:46:05 -0400

    Update testResults.dart

--- lib/testResults.dart
+++ lib/testResults.dart
@@ -7,13 +7,33 @@ class TestResults extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Test Results'),
       ),
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+              leading: Text('1'),
+              title: Text("White blood cells"),
+              subtitle: Text("14.2")),
+          ListTile(
+              leading: Text('2'),
+              title: Text("Hemoglobin"),
+              subtitle: Text("13.6")),
+          ListTile(
+              leading: Text('3'),
+              title: Text("Hematocrit"),
+              subtitle: Text("40.1")),
+          ListTile(
+              leading: Text('4'),
+              title: Text("Platelets"),
+              subtitle: Text("247")),
+          ListTile(
+              leading: Text('5'), title: Text("Sodium"), subtitle: Text("137")),
+          FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go back!'),
+          ),
+        ],
       ),
     );
   }
