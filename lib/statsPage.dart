799fe9e3ecab2ddad77f175152d60303e0c2163b
James Staszel <staszel@umich.edu>
2020-11-15 22:09:21 -0500

    stats page update

--- lib/statsPage.dart
+++ lib/statsPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:RESP2/userData.dart';
 
 class StatsPage extends StatefulWidget {
   StatsPage({Key key, this.title}) : super(key: key);
@@ -12,13 +13,32 @@ class StatsPage extends StatefulWidget {
 class _StatsPageState extends State<StatsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: (ListView(children: [
-      ListTile(title: Text("Cases Attempted"), subtitle: Text("45")),
-      ListTile(title: Text("Correct Diagnoses"), subtitle: Text("35")),
-      ListTile(title: Text("Accuracy"), subtitle: Text("78.5%")),
-      ListTile(title: Text("Average Score"), subtitle: Text("5865")),
-      ListTile(title: Text("Most Misdiagnosed"), subtitle: Text("COPD")),
-    ])));
+    return new FutureBuilder(
+        future: getStatistics(),
+        builder: (BuildContext context, AsyncSnapshot<Map<String, int>> data) {
+          return Scaffold(
+              body: (ListView(children: [
+            ListTile(
+                title: Text("Total correct"),
+                subtitle: Text(data.data['numCorrect'].toString())),
+            ListTile(
+                title: Text("Cases attempted"),
+                subtitle: Text(data.data['attempted'].toString())),
+            /*ListTile(
+                title: Text("Accuracy"),
+                subtitle: Text(
+                    (data.data['attempted'] / data.data['numCorrect'])
+                        .toString())),*/
+            ListTile(
+                title: Text("Longest streak"),
+                subtitle: Text(data.data['longestStreak'].toString())),
+            ListTile(
+                title: Text("Most misdiagnosed"),
+                subtitle: Text(data.data['misdiagnosed'].toString())),
+            ListTile(
+                title: Text("Current points"),
+                subtitle: Text(data.data['storePoints'].toString())),
+          ])));
+        });
   }
 }
